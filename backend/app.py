ef768f0a7a8c99317c5153c81de90c82870b98cf
Tibor Kiss <32801432+the1bit@users.noreply.github.com>
2026-06-23 11:03:37 +0000

    Init

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Helló Világ! Ez egy Cloud Run alkalmazás, Cloud Build segítségével telepítve.'
+
+if __name__ == "__main__":
+    # A Cloud Run a PORT környezeti változóban adja meg a portot.
+    port = int(os.environ.get('PORT', 8080))
+    # Ne használd a debug=True-t éles környezetben!
+    app.run(host='0.0.0.0', port=port)
